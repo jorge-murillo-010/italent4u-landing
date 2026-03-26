91aa79fb4d535b7e35dc3ad1a115575ccd6e9b8d
Giftyarkoh <garkoh@byupathway.edu>
2026-03-26 10:38:26 +0000

    Added pages and updated styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("loaded");
+// footer //
+const currentYear = new Date().getFullYear();
+const yearElem = document.getElementById("currentyear");
+if (yearElem) yearElem.innerHTML = currentYear;
+
+const lastModified = document.lastModified;
+const modifiedElem = document.getElementById("lastModified");
+if (modifiedElem) modifiedElem.innerHTML = "Last Modified: " + lastModified;
+
+// header //
+const mainnav = document.querySelector('.navbar');
+const hambutton = document.querySelector('#menu');
+
+if (mainnav && hambutton) {
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('open');
+        hambutton.classList.toggle('open');
+    });
+}
